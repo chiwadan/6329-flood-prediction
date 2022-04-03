7c8b45c946183dd76256895f297c9ff7df65b6b2
chiwadan <36788446+chiwadan@users.noreply.github.com>
2022-04-03 21:32:34 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,8 +14,6 @@ def hello_world():
     else:
         year = request.form['YEAR']
         annual = request.form['ANNUAL']
-        print('year')
-        print('annual')
         model = load('app/flood-prediction.joblib')
         np_arr = np.array([year, annual])
         predictions = model.predict([np_arr])  
